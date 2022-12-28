3b1abec8fdb305799c608ee56a0454777ce5b337
davidfox <foxy1987@gmail.com>
2022-12-27 23:50:02 -0700

    initial commit

---
+++ mnist/build_and_push.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 880572800141.dkr.ecr.us-west-1.amazonaws.com
+
+# [ $? -eq 0 ] && docker build -t mnist-model .
+# [ $? -eq 0 ] && docker tag mnist-model:latest 880572800141.dkr.ecr.us-west-1.amazonaws.com/mnist-model:latest
+# [ $? -eq 0 ] && docker push 880572800141.dkr.ecr.us-west-1.amazonaws.com/mnist-model:latest
+
+
+docker login --username=foxy7887 -p MGSGiw550!
+
+image_name=foxy7887/mnist-model
+image_tag=v31
+full_image_name=${image_name}:${image_tag}
+
+cd "$(dirname "$0")" 
+docker build -t "${full_image_name}" .
+docker push "$full_image_name"
+
+# Output the strict image name, which contains the sha256 image digest
+docker inspect --format="{{index .RepoDigests 0}}" "${full_image_name}"
+
+docker image rm "${full_image_name}"
